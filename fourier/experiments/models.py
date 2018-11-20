de85356d057ad71e68b392c524b89df9a48fb875
schopra8 <schopra8@stanford.edu>
2018-11-20 14:11:08 -0800

    minor fixes

--- fourier/experiments/models.py
+++ fourier/experiments/models.py
@@ -174,7 +174,7 @@ def fourier_encoding(xx_positions, yy_positions):
     yy_positions_npy = yy_positions.numpy()
 
     def get_sinusoid_encoding_table(n_position, d_hid):
-	    ''' Sinusoid position encoding table '''
+	''' Sinusoid position encoding table '''
 
         def cal_angle(position, hid_idx):
             return position / np.power(10000, 2 * (hid_idx // 2) / d_hid)
@@ -185,7 +185,6 @@ def fourier_encoding(xx_positions, yy_positions):
         sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in range(n_position)])
         sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
         sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
-
         return sinusoid_table
 
     i_vec = get_sinusoid_encoding_table(image_size, d_hid)
